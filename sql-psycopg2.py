4bd7a5beb0f983e2f55f9d19091eace0955e4549
2ndborn <>
2023-11-21 07:08:15 +0000

    installing the psycopg library

---
+++ sql-psycopg2.py
@@ -0,0 +1,39 @@
+import psycopg2
+
+
+# connect to the "chinook" data# base
+connection = psycopg2.connect(database="chinook")
+
+# Build a cursor object of the database
+cursor = connection.cursor()
+
+# Query 1 - select all records from the "Artist" table
+# cursor.execute('SELECT * FROM "Artist"')
+
+# Query 2 - select only the "Name" column from the "Artist" table
+# cursor.execute('SELECT "Name" FROM "Artist"')
+
+# Query 3 - select only "Queen" from the "Artist" table
+# cursor.execute('SELECT * FROM "Artist" WHERE "Name" = %s', ["Alanis Morissette"])
+
+# Query 4 - select only "ArtistId" from the "Artist" table
+# cursor.execute('SELECT * FROM "Artist" WHERE "ArtistId" = %s', [51])
+
+# Query 4 - select only "ArtistId" from the "Artist" table
+# cursor.execute('SELECT * FROM "Album" WHERE "ArtistId" = %s', [51])
+
+# Query 4 - select only "ArtistId" from the "Artist" table
+cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["Alanis Morissette"])
+
+# fetch the results (multiple)
+results = cursor.fetchall()
+
+# fetch the results (single)
+# results = cursor.fetchone()
+
+# close the connection
+connection.close()
+
+# print result
+for result in results:
+    print(result)
